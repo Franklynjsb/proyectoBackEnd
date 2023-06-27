381876d647085a7ccdaa5e19920310b00e3b351a
Frank♥ <98553892+Franklynjsb@users.noreply.github.com>
2023-06-27 18:13:43 -0300

    Antmatcher

--- src/main/java/com/francoquijada/fjq/Security/MainSecurity.java
+++ src/main/java/com/francoquijada/fjq/Security/MainSecurity.java
@@ -6,6 +6,7 @@ import com.francoquijada.fjq.Security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -24,7 +25,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserDetailsImpl userDetailsServicesImpl;
-    
+
     @Autowired
     JwtEntryPoint jwtEntryPoint;
 
@@ -42,7 +43,8 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
